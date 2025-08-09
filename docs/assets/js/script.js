ed4c1e1aca0130604e70da9b00057abe9f9e8365
Franco Lesme <franco.lesme2006@gmail.com>
2025-08-08 22:01:38 -0300

    avances en sección equipo y tecnología
    Co-Authored-By: Thiago Agustin Albornoz <101573327+albornozthiagoo@users.noreply.github.com>

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,7 +1,6 @@
 /**
  * script.js - FutureNav + Formulario con Resend
  */
-
 document.addEventListener('DOMContentLoaded', () => {
   // --- Código original ---
   const navbar = document.getElementById('navbar');
